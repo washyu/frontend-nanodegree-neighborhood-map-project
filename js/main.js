2c696cd72ecea7068f8774888f0e7baab87776ed
washyu <washyu@hotmail.com>
2015-02-13 01:12:18 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,7 +37,6 @@ var Model = function() {
 
 		this.mapOptions = {
 				disableDefaultUI: true,
-				panControl: false,
 				center: new google.maps.LatLng(location.lat, location.lng),
 				zoom: this.zoom,
 				draggable: false
