67e3245344b8c5ec6e6f9b06f13c16d10d75af0a
washyu <washyu@hotmail.com>
2015-02-22 02:23:59 -0800

    Made some changes the content createtion:  Created a manual subscription to format the content when the api data updates.

--- js/main.js
+++ js/main.js
@@ -54,7 +54,6 @@ var Model = function() {
 		var map = new aMap();
 		return map.map;
 	};
-
 };
 
 var ViewModel = function() {
@@ -63,15 +62,22 @@ var ViewModel = function() {
 	self.myMap = self.model.getMap();
 	self.myMarkers = ko.observableArray([]);
 
+	google.maps.InfoWindow.prototype.isOpen = function() {
+		return(this.getMap() !== null && this.getMap() !== undefined);
+	};
+
+	self.infoWindow = new google.maps.InfoWindow({
+		content: "Loading FourSquare data..."
+	});
+
 	self.markerObject = function(markerData) {
 		this.markerData = markerData;
 		this.name = this.markerData.name;
 		this.searchString = this.markerData.searchString;
 		this.hovered = ko.observable(false);
 		this.styleIcon = new StyledIcon(StyledIconTypes.MARKER,{color:"#f00"});
-		this.infoWindow = new google.maps.InfoWindow({
-			content: "Loading FourSquare data..."
-		});
+		this.s4VenueObject = ko.observable();
+		this.s4TipsObject = ko.observable();
 
 		this.marker = new StyledMarker({
 				styleIcon: this.styleIcon,
@@ -92,6 +98,10 @@ var ViewModel = function() {
 		google.maps.event.addListener(this.marker, 'click', function() {
 			self.mouseClick(this);
 		}.bind(this));
+
+		this.s4VenueObject.subscribe(function(newValue){
+			self.infoWindow.setContent("this is a test: " + newValue.name);
+		});
 
 		this.getLatLng = function() {
 			return this.markerData.latLng;
@@ -117,15 +127,20 @@ var ViewModel = function() {
 	};
 
 	self.mouseClick = function(marker) {
-		marker.infoWindow.content = self.get4sinfo(marker);
-		marker.infoWindow.open(self.myMap, marker.marker);
+		if(self.infoWindow.isOpen())
+		{
+			self.infoWindow.close();
+			self.infoWindow.setContent("Loading FourSquare data...");
+		}
+		self.load4sInfo(marker);
+		self.infoWindow.open(self.myMap, marker.marker);
 	};
 
 	self.toggleOpen = function() {
 		$("#panel").toggleClass("open");
 	};
 
-	self.get4sinfo = function(marker){
+	self.load4sInfo = function(marker){
 		var url = 'https://api.foursquare.com/v2/venues/search?'
 		 + 'client_id=JBYUIJHUTG5EH0UGEXNEOF403IAEACBBNLM1TFPL4OC2PBM1'
 		 + '&client_secret=SQT2NBDTODIVCOFWDGKHL1NCUCZ4TN045RR1EXVDUPQGCJGT&v=20130815'
@@ -133,20 +148,15 @@ var ViewModel = function() {
 		 + '&query=\'' + marker.name + '\''
 		 + '&limit=1';
 
-		 $.getJSON(url)
+		$.getJSON(url)
 		 	.done(function(response){
-		 		var tempString = '<p>FourSquare Info:<br>';
-		 		var venueObject = response.response.venues[0];
-		 		var venueId = venueObject.id;
-		 		tempString += 'Name: ' + ((venueObject.name !== null && venueObject.name !== undefined) ? venueObject.name : marker.name) + '<br>';
-		 		tempString += 'Phone: ' + ((venueObject.contact.formatedPhone !== null && venueObject.contact.formatedPhone !== undefined) ? venueObject.contact.formatedPhone : 'No Number Listed') + '<br>';
-		 		marker.infoWindow.setContent(tempString);
+		 		marker.s4VenueObject(response.response.venues[0]);
 		 	})
 		 	.fail(function(){
-		 		marker.infoWindow.setContent('There was an issue getting the FourSquare data.');
+		 		console.log('There was an issue getting the FourSquare data.');
 		 	});
 	};
-}
+};
 
 $(document).ready(function() {
 	ko.applyBindings(new ViewModel());
